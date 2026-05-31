dcfd5f700959abc0b5ed4415d84333a1ac9e20de
Nikita Bardatskii <bardanik@fit.cvut.cz>
2026-05-31 18:21:06 +0200

    Change style, having rounded borders around items

--- personal-website/src/components/experience/ExperienceTree.tsx
+++ personal-website/src/components/experience/ExperienceTree.tsx
@@ -5,27 +5,42 @@ import './ExperienceTree.css'
 type Props = {
     nodes: ExperienceNode[];
     depth?: number;
+    parentKey: string;
 };
 
-export function ExperienceTree({nodes, depth = 0}: Props) {
+export function ExperienceTree({nodes, depth = 0, parentKey = ""}: Props) {
 
     return (
         <div className="experience-tree">
             {nodes.map((node) => (
-                <div
-                    key={node.title}
-                    className="experience-node"
-                    style={{marginLeft: `${depth * 1.5}rem`}}
-                >
-                    {node.type === "folder" ? (
-                        <details open>
+                node.type === "folder" ? (
+                    <div
+                        key={`${parentKey}/${node.title}`}
+                        className={depth === 0
+                            ? "experience-folder-wrapper-top-level"
+                            : "experience-folder-wrapper"}
+                        style={{marginLeft: `${depth * 1.5}rem`}}
+                    >
+                        <details>
                             <summary>{node.title}</summary>
-                            <ExperienceTree nodes={node.children} depth={depth + 1}/>
+                            <ExperienceTree nodes={node.children}
+                                            depth={depth + 1}
+                                            parentKey={`${parentKey}/${node.title}`}/>
                         </details>
-                    ) : (
+                    </div>
+                ) : (
+                    <div
+                        key={`${parentKey}/${node.title}`}
+                        className={depth === 0
+                            ? "experience-item-wrapper-top-level"
+                            : "experience-item-wrapper"}
+                        style={{marginLeft: `${depth * 1.5}rem`}}
+                    >
                         <ExperienceItem item={node}/>
-                    )}
-                </div>
+                    </div>
+                )
+
+
             ))}
         </div>
     );
