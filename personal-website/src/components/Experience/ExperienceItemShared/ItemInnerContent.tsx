d8f417eedf0e7035ad40306a4b7b7c1b6e343e42
Nikita Bardatskii <bardanik@fit.cvut.cz>
2026-06-19 15:36:09 +0200

    Add media gallery button

--- personal-website/src/components/Experience/ExperienceItemShared/ItemInnerContent.tsx
+++ personal-website/src/components/Experience/ExperienceItemShared/ItemInnerContent.tsx
@@ -2,6 +2,7 @@ import ReactMarkdown from "react-markdown";
 import {dedent} from "ts-dedent";
 import type {ExperienceItem} from "../../../types/ExperienceNode.ts";
 import './ItemInnerContent.css'
+import {MediaGalleryButton} from "./MediaContent.tsx";
 
 type Props = {
     item: ExperienceItem;
@@ -27,6 +28,10 @@ export function ItemInnerContent({item, contentRef}: Props) {
                     </ReactMarkdown>
                 </div>
             )}
+
+            {item.media_files && item.media_files.length > 0 &&
+                <MediaGalleryButton media={item.media_files}/>
+            }
 
             {item.postscriptum && (
                 <div className="experience-tree-item-postscriptum">
